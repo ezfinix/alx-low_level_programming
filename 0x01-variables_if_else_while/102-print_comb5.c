97a1102bfff77a23a9c51442f74221dd4b38153d
Ezeh Chidiebere Paul <ezberek008@gmail.com>
2023-02-17 20:41:48 -0800

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - print pairs of double digit combos
+ *
+ * Description: print pairs of double digit combos
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int i, j, k, m;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-	g = 48;
-	while (g < 58)
+	i = 48;
+	while (i < 58)
 	{
-		e = 48;
-		while (e < 58)
+		j = 48;
+		while (j < 58)
 		{
-			i = 48;
-			while (i < 58)
+			m = j + 1;
+			k = i;
+			while (k < 58)
 			{
-				op1 = (h * 10) + g;
-				op2 = (e * 10) + i;
-				if (op1 < op2)
+				while (m < 58)
 				{
-					putchar(h);
-					putchar(g);
-					putchar(' ');
-					putchar(e);
 					putchar(i);
-					if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
-					putchar(',');
-					putchar(' ');
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if (i < 57 || j < 56 || k < 57 || m < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					m++;
 				}
-				i++;
+				m = 48;
+				k++;
 			}
-			e++;
+			j++;
 		}
-		g++;
+		i++;
 	}
-	h++;
-}
-putchar('\n');
-return (0);
+	putchar(10);
+	return (0);
 }
