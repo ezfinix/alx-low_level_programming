db421a0bb3d36d0e4596059fa56340aeefa36a4c
Ezeh Chidiebere <ezberek008@gmail.com>
2023-02-22 02:02:10 +0100

    10 x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int ntimes = 0;
+	char letter = 'a';
+
+	while (ntimes < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		ntimes++;
+	}
+}
